1a895b991353794248f3cc43a3b5e6990d01aeaf
kongzilla-ks <kongzilla@proton.me>
2025-02-12 00:03:35 +0100

    updated scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,11 @@ if [ -z "$1" ]
 fi
 IDENTITY="--identity kong"
 PRINCIPAL_ID=$(dfx identity ${IDENTITY} get-principal)
+KONG_DATA_CANISTER_ID='cbefx-hqaaa-aaaar-qakrq-cai'
 
 dfx deploy ${NETWORK} ${IDENTITY} event_store --argument "(
     record {
-        push_events_whitelist = vec { principal \"${PRINCIPAL_ID}\" };
+        push_events_whitelist = vec { principal \"${KONG_DATA_CANISTER_ID}\" };
         read_events_whitelist = vec { principal \"${PRINCIPAL_ID}\"};
-        time_granularity = opt (2000:nat64);
     }
 )"
--- scripts/push_events.sh
+++ scripts/push_events.sh
@@ -11,13 +11,14 @@ NOW=$(date +%s%3N)
 
 dfx canister call ${NETWORK} ${IDENTITY} ${EVENT_STORE_CANISTER_ID} push_events "(
     record {
-        events = vec { record {
-            idempotency_key = (3: nat);
-            name = \"KongSwap\";
-            source = opt variant { Public=\"${PRINCIPAL_ID}\" };
-            user = opt variant { Public=\"${PRINCIPAL_ID}\" };
-            timestamp = (${NOW}: nat64);
-            payload = blob \"{prop1 : \\\"hello world\\\"}\";
+        events = vec {
+            record {
+                idempotency_key = (3: nat);
+                name = \"KongSwap\";
+                source = opt variant { Public=\"${PRINCIPAL_ID}\" };
+                user = opt variant { Public=\"${PRINCIPAL_ID}\" };
+                timestamp = (${NOW}: nat64);
+                payload = blob \"{prop1 : \\\"hello world\\\"}\";
             };
         }
     }
